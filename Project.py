9940f2f568b76919b919ddd504be002bafadc889
vvthanh-hcmus <vv.thanhhh@gmail.com>
2025-06-13 10:17:39 +0700

    Thêm Project.py vào repo GitHub

--- Project.py
+++ Project.py
@@ -1,15 +1,16 @@
 import cv2
-import pyttsx3
 import threading
 import time
 import numpy as np
 from scipy.spatial import distance
 import os
+import serial
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import mediapipe as mp
 
-# Text-to-Speech setup
-engine = pyttsx3.init()
+# Connect Serial
+ser = serial.Serial('COM6', 115200, timeout=1)
 
 # MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
@@ -33,18 +34,10 @@ def get_MAR(mouth):
     C = distance.euclidean(mouth[0], mouth[4])
     return (A + B) / (2.0 * C)
 
-# Alert function
-def alert_loop():
-    while alarm_on_flag[0]:
-        engine.say("Alert! Wake up!")
-        engine.runAndWait()
-        time.sleep(1.5)
-
 # State variables
 COUNTER = 0
 ALARM_FRAME_THRESHOLD = 15
 alarm_on_flag = [False]
-alarm_thread = None
 
 # Indexes for face landmarks from MediaPipe
 LEFT_EYE_IDX = [362, 385, 387, 263, 373, 380]
@@ -55,7 +48,7 @@ MOUTH_IDX = [78, 81, 13, 311, 308, 402, 14, 178]  # Approximate inner mouth
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Không lấy được khung hình.")
+        print("Cannot connect to the frame.")
         break
 
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -65,7 +58,7 @@ while True:
         for face_landmarks in results.multi_face_landmarks:
             h, w, _ = frame.shape
 
-            # Extract eye landmarks
+            # Extract eye & mouth landmarks
             leftEye = [(int(face_landmarks.landmark[i].x * w), int(face_landmarks.landmark[i].y * h)) for i in LEFT_EYE_IDX]
             rightEye = [(int(face_landmarks.landmark[i].x * w), int(face_landmarks.landmark[i].y * h)) for i in RIGHT_EYE_IDX]
             mouth = [(int(face_landmarks.landmark[i].x * w), int(face_landmarks.landmark[i].y * h)) for i in MOUTH_IDX]
@@ -79,7 +72,7 @@ while True:
 
             drowsy_detected = False
 
-            # Buồn ngủ
+            # Sleepiness Detection
             if EAR < 0.25:
                 COUNTER += 1
                 if COUNTER >= ALARM_FRAME_THRESHOLD:
@@ -89,26 +82,26 @@ while True:
             else:
                 COUNTER = 0
 
-            # Ngáp
+            # Yawn
             if MAR > 0.6:
                 cv2.putText(frame, "YAWNING DETECTED", (50, 150),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
                 drowsy_detected = True
 
-            # Cảnh báo
+            # Alert Serial
             if drowsy_detected and not alarm_on_flag[0]:
                 alarm_on_flag[0] = True
-                alarm_thread = threading.Thread(target=alert_loop)
-                alarm_thread.daemon = True
-                alarm_thread.start()
-            elif not drowsy_detected:
+                ser.write(b'1')  
+            elif not drowsy_detected and alarm_on_flag[0]:
                 alarm_on_flag[0] = False
+                ser.write(b'0')  
 
     cv2.imshow("Driver Monitor (MediaPipe)", frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
-        alarm_on_flag[0] = False
+        ser.write(b'0') 
         break
 
 cap.release()
 cv2.destroyAllWindows()
+ser.close()
